d9fa6a6c7cf5a8b52b12c21a6cb45cbf81d0de6b
congp <congp@miichisoft.net>
2022-05-12 14:19:46 +0700

    add app tracking transparency

---
+++ demo Other/AppTrackingTransparencyDemo/AppTrackingTransparencyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTrackingTransparencyDemo
+//
+//  Created by Miichi on 5/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
